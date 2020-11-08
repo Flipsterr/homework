4e8d8f9b6f387a6b00130db3cd83691b5a6b70aa
Flipsterr <filip.von.bromssen@gmail.com>
2020-11-08 16:48:43 +0100

    vecka45!

---
+++ 45/2/script.js
@@ -0,0 +1,5 @@
+function wordLength(str){
+    return str.length
+}
+
+console.log(wordLength("Kardemummalängd"))
---
+++ 45/3/script.js
@@ -0,0 +1,6 @@
+function wordCount(str){
+    var words = str.split(" ")
+    return words.length
+}
+
+console.log(wordCount("Jag älskar kardemummalängd"))
